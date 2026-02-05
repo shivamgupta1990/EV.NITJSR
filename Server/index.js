ee647002e71efdcd9a0d3c1082b8c006a875c1f0
Shivam Gupta <shivamgupta1700000@gmail.com>
2026-02-05 13:38:24 +0530

    cors

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      // "https://ev-nitjsr-pumk.vercel.app" // Replace with your actual vercel link
+      "https://ev-nitjsr-pumk.vercel.app" // Replace with your actual vercel link
     ],
     credentials: true
   })
